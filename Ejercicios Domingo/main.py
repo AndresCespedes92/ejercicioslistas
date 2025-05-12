2f6ef68217da301353491df4b42d2a7f6360c338
Andres Cespedes <cespedesandresr@gmail.com>
2025-05-12 20:12:27 -0300

    Tercer release

--- Ejercicios Domingo/main.py
+++ Ejercicios Domingo/main.py
@@ -1,4 +1,21 @@
-from ej1 import agregar
+from agregar import agregar
+from insertar import insertar
+from obtener_indice import obtener_indice
+from eliminar_ultimo import eliminar
+from eliminar_primer_instancia import eliminar_primer_instancia
+from eliminar_todos import eliminar_todos
+from vaciar_lista import vaciar_lista
 
 print(agregar([1, 2, 3], 4))
 
+print(insertar([1, 2, 3], 4, 0))
+
+print(obtener_indice([1, 2, 3], 4))
+
+print(eliminar([1, 2, 3]))
+
+print(eliminar_primer_instancia([1, 2, 3], 4))
+
+print(eliminar_todos([1, 2, 3], 4))
+
+print(vaciar_lista([1, 2, 3]))
